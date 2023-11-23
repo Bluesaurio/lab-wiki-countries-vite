844160be45d7d91de696ea72372fbe84c3af03de
Bluesaurio <adriansiquiervalles@gmail.com>
2023-11-23 19:54:41 +0100

    lab done

--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -1,3 +1,52 @@
-function CountryDetails() {}
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+function CountryDetails() {
+  const params = useParams();
+  console.log(params.alphaCode);
+
+  const [countryDetails, setCountryDetails] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://ih-countries-api.herokuapp.com/countries/${params.alphaCode}`
+      )
+      .then((response) => {
+        console.log(response);
+        setCountryDetails(response.data);
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading === true) {
+    return <h3>Cargando datos...</h3>;
+  }
+
+  const countryBorders = countryDetails.borders.map((eachBorder) => {
+    return eachBorder;
+  });
+
+  return (
+    <header>
+      <p style={{ fontSize: "20px", fontWeight: "bold" }}>Country Details</p>
+      <h4>Name: {countryDetails.name.common}</h4>
+      <h5>Capital: {countryDetails.capital}</h5>
+      <p>Area: {countryDetails.area} km2</p>
+      {/* <p>
+        Borders:
+        {
+          <Link to={`/country/${countryBorders}`}>
+            <li>{countryBorders}</li>
+          </Link>
+          Falta desplegar la lista de países con los que hace frontera
+        }
+      </p> */}
+    </header>
+  );
+}
 
 export default CountryDetails;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,41 @@
-function HomePage() {}
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+function HomePage() {
+  const [countryList, setCountryList] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get("https://ih-countries-api.herokuapp.com/countries")
+      .then((response) => {
+        console.log(response.data);
+        setCountryList(response.data);
+        setIsLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  if (isLoading === true) {
+    return <h3>...Loading...</h3>;
+  }
+
+  return (
+    <header>
+      <h1>List of Countries</h1>
+      {countryList.map((eachCountry) => {
+        return (
+          <Link
+            key={eachCountry.alpha3Code}
+            to={`/country/${eachCountry.alpha3Code}`}
+          >
+            <p>{eachCountry.name.common}</p>
+          </Link>
+        );
+      })}
+    </header>
+  );
+}
 
 export default HomePage;
